e5c9e23bd571734394708caeb8eace0c3be1cb8f
Ilse Centeno <Ilse Centeno>
2017-06-01 12:02:12 -0500

    commit master

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+// $("#enviar").click(publicacion);{
+
+// }
+
+// var publicacion= function(){
+
+// 	var usuario = $("#usuario").val();
+//   	var texto = $("#texto").val();
+//   	var newTwitte = $("#nuevo").text("@" + usuario + ":" + " " +texto;);
+
+//   	var newPost = $("<section></section>"); 
+//     var parrafo= $("<p></p>");
+//     var linea= $("<hr/>");
+   
+
+ 
+//     newTwitte.append= newPost;
+    // newPost.append(parrafo);
+    // newPost.append(linea);
+    // newTwitte.append(newPost);
+
+ $("#enviarBtn").click(publicacion);
+
+var publicacion= function() {
+
+  var p = $("<p></p>");
+  var inputValue = $("#texto").val();
+  var t = text(inputValue);
+  p.append(t);
+ 
+}
+
+
+
+
+
+
